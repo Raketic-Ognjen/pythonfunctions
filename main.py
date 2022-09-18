258f37bc4d1fee06c15c0cde55c552c63f53640c
ogi <ognjen.raketic@gmail.com>
2022-09-18 22:09:19 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+
+
+
+def func2(a, b, c):
+   if c == True:
+      return a
+   else:
+      return b
+func2('Irena', 'Ognjen', True)
+
+"""
+Boolean 1
+"""
+
+def func1(a):
+   if a == True:
+      return 'Inside'
+   elif a == False:
+      return 'Outside'
+func1(False)
+"""
+Boolean 2
+"""
+
+def myfunc():
+   print('Hello World')
+
+
+myfunc()
+"""
+Printing hello world with function
+"""
+
+
+def my_func(name):
+   print(f'my name is {name}'.format(name))
+
+
+my_func('Ogi')
+
+"""
+Printing name
+"""
